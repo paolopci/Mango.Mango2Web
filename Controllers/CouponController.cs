debd0ec1dd716bc5bb86241414238552cdf27320
PaoloPci <paolopci@yahoo.it>
2024-02-16 16:35:35 +0100

    012 Create Coupon View

--- Controllers/CouponController.cs
+++ Controllers/CouponController.cs
@@ -28,5 +28,10 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
     }
 }
