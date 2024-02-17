f75dd7f123c1ade898999ece9e3d36aa64e7596c
PaoloPci <paolopci@yahoo.it>
2024-02-17 19:24:33 +0100

    016 Toastr Notifications Complete 03 - SEction

--- Controllers/CouponController.cs
+++ Controllers/CouponController.cs
@@ -24,6 +24,10 @@ namespace Mango.Web.Controllers
             {
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
 
 
             return View(list);
@@ -42,7 +46,13 @@ namespace Mango.Web.Controllers
                 ResponseDto? response = await _couponService.CreateCouponAsync(couponDto);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Coupon created successfully";
                     return RedirectToAction(nameof(CouponIndex));
+
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
                 }
             }
             // se modelState is not valid ritorno alla view con couponDto per le correzioni
@@ -70,12 +80,12 @@ namespace Mango.Web.Controllers
             ResponseDto? response = await _couponService.DeleteCouponAsync(model.CouponId);
             if (response != null && response.IsSuccess)
             {
-                 TempData["success"] = "Coupon deleted successfully";
+                TempData["success"] = "Coupon deleted successfully";
                 return RedirectToAction(nameof(CouponIndex));
             }
             else
             {
-                TempData["error"] = response?.Message; 
+                TempData["error"] = response?.Message;
             }
 
             // se modelState is not valid ritorno alla view con couponDto per le correzioni
