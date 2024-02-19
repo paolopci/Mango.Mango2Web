b19f9fa9d4e396d0961c200bd67d0439166611dd
PaoloPci <paolopci@yahoo.it>
2024-02-19 14:40:58 +0100

    008 Token Provider Services

--- Program.cs
+++ Program.cs
@@ -16,9 +16,10 @@ builder.Services.AddHttpClient<IAuthService, AuthService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IBaseService, BaseService>();
-builder.Services.AddScoped<ICouponService, CouponService >();
-builder.Services.AddScoped<IAuthService, AuthService >();
+builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
