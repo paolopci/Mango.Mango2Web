fac41fde095e70bc8384a520b2b699bba91b0659
PaoloPci <paolopci@yahoo.it>
2024-02-19 09:43:18 +0100

    001 Add DTO's in Web Project

---
+++ Models/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Web.Models
+{
+    public class LoginResponseDto
+    {
+        //todo: Se un utente riesce a loggarsi allora ritorna UserDto+Token !!!!
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
