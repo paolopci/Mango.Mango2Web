0d675ca94bdb593c54067252870cca14873f1ddc
PaoloPci <paolopci@yahoo.it>
2024-02-28 16:32:58 +0100

    007 Manage Stripe Coupons

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,11 +1,11 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using Mango.Web.Models;
-using Mango.Web.Models.Dto;
+﻿using Mango.Web.Models;
+using Mango.Web.Service;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
 {
@@ -13,7 +13,6 @@ namespace Mango.Web.Controllers
     {
         private readonly ICartService _cartService;
         private readonly IOrderService _orderService;
-
         public CartController(ICartService cartService, IOrderService orderService)
         {
             _cartService = cartService;
@@ -23,119 +22,130 @@ namespace Mango.Web.Controllers
         [Authorize]
         public async Task<IActionResult> CartIndex()
         {
-            return View(await LoadCartDtoBaseOnLoggedInUser());
+            return View(await LoadCartDtoBasedOnLoggedInUser());
         }
-
-
-        [Authorize]
-        public async Task<IActionResult> Confirmation(int orderId)
-        {
-            return View(orderId);
-        }
-
-
 
         [Authorize]
         public async Task<IActionResult> Checkout()
         {
-            return View(await LoadCartDtoBaseOnLoggedInUser());
+            return View(await LoadCartDtoBasedOnLoggedInUser());
         }
-
         [HttpPost]
         [ActionName("Checkout")]
         public async Task<IActionResult> Checkout(CartDto cartDto)
         {
-            CartDto cart = await LoadCartDtoBaseOnLoggedInUser();
+
+            CartDto cart = await LoadCartDtoBasedOnLoggedInUser();
             cart.CartHeader.Phone = cartDto.CartHeader.Phone;
             cart.CartHeader.Email = cartDto.CartHeader.Email;
             cart.CartHeader.Name = cartDto.CartHeader.Name;
 
             var response = await _orderService.CreateOrder(cart);
-            OrderHeaderDto orderHeaderDto =
-                JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+            OrderHeaderDto orderHeaderDto = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
 
             if (response != null && response.IsSuccess)
             {
-                // get stripoe session and redirect to strip to place order
-            }
+                //get stripe session and redirect to stripe to place order
+                //
+                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
 
+                StripeRequestDto stripeRequestDto = new()
+                {
+                    ApprovedUrl = domain + "cart/Confirmation?orderId=" + orderHeaderDto.OrderHeaderId,
+                    CancelUrl = domain + "cart/checkout",
+                    OrderHeader = orderHeaderDto
+                };
+
+                var stripeResponse = await _orderService.CreateStripeSession(stripeRequestDto);
+                StripeRequestDto stripeResponseResult = JsonConvert.DeserializeObject<StripeRequestDto>
+                                            (Convert.ToString(stripeResponse.Result));
+                Response.Headers.Add("Location", stripeResponseResult.StripeSessionUrl);
+                return new StatusCodeResult(303);
+
+
+
+            }
             return View();
         }
 
+        public async Task<IActionResult> Confirmation(int orderId)
+        {
+            ResponseDto? response = await _orderService.ValidateStripeSession(orderId);
+            if (response != null & response.IsSuccess)
+            {
 
+                OrderHeaderDto orderHeader = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+                if (orderHeader.Status == SD.Status_Approved)
+                {
+                    return View(orderId);
+                }
+            }
+            //redirect to some error page based on status
+            return View(orderId);
+        }
 
-        public async Task<IActionResult> Remove(int CartDetailsId)
+        public async Task<IActionResult> Remove(int cartDetailsId)
         {
             var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
-            ResponseDto? response = await _cartService.RemoveFromCartAsync(CartDetailsId);
-            if (response != null && response.IsSuccess)
+            ResponseDto? response = await _cartService.RemoveFromCartAsync(cartDetailsId);
+            if (response != null & response.IsSuccess)
             {
                 TempData["success"] = "Cart updated successfully";
                 return RedirectToAction(nameof(CartIndex));
             }
-
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> ApplyCoupon(CartDto cartDto)
         {
-
+            
             ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
-            if (response != null && response.IsSuccess)
+            if (response != null & response.IsSuccess)
             {
                 TempData["success"] = "Cart updated successfully";
                 return RedirectToAction(nameof(CartIndex));
             }
-
             return View();
         }
-
 
         [HttpPost]
         public async Task<IActionResult> EmailCart(CartDto cartDto)
         {
-            // ritorno il cart dell'utente loggato
-            CartDto cart = await LoadCartDtoBaseOnLoggedInUser();
-            // salvo la sua email
+            CartDto cart = await LoadCartDtoBasedOnLoggedInUser();
             cart.CartHeader.Email = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Email)?.FirstOrDefault()?.Value;
             ResponseDto? response = await _cartService.EmailCart(cart);
-            if (response != null && response.IsSuccess)
+            if (response != null & response.IsSuccess)
             {
                 TempData["success"] = "Email will be processed and sent shortly.";
                 return RedirectToAction(nameof(CartIndex));
             }
-
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> RemoveCoupon(CartDto cartDto)
         {
-
             cartDto.CartHeader.CouponCode = "";
             ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
-            if (response != null && response.IsSuccess)
+            if (response != null & response.IsSuccess)
             {
                 TempData["success"] = "Cart updated successfully";
                 return RedirectToAction(nameof(CartIndex));
             }
-
             return View();
         }
 
 
-
-        private async Task<CartDto> LoadCartDtoBaseOnLoggedInUser()
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
             var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
             ResponseDto? response = await _cartService.GetCartByUserIdAsync(userId);
-            if (response != null && response.IsSuccess)
+            if(response!=null & response.IsSuccess)
             {
                 CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
                 return cartDto;
             }
-
             return new CartDto();
         }
     }
