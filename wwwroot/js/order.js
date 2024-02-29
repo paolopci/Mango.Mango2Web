421ca32db08b4b1783493ba6ef14ab88202aeed1
PaoloPci <paolopci@yahoo.it>
2024-02-29 12:03:32 +0100

    005 Configure Database Endpoint and initial configure dataTables

---
+++ wwwroot/js/order.js
@@ -0,0 +1,12 @@
+﻿var dataTable;
+$(document).ready(function () {
+    dataTable = $('#tblData').dataTable({
+        "ajax": {
+            url: "/order/getall"  // order è il nome del OrderController and getall del ActionMethod!!!
+        },
+        "columns": [
+            { data: 'orderheaderid', "width": "5%"},  // orderheaderId è il nome della columns indice nella tabella OrderHeaderDto!!!
+            { data: 'email', "width": "25%"} // sempre dalla OrderHeaderId il campo email
+        ]
+    })
+})
